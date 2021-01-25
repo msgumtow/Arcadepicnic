f7845f14b7da82150049ebbc1662e15ec7ee9937
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-01-25 13:38:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Person.overlapsWith(chicken)) {
+        info.changeScoreBy(1)
+        chicken.destroy()
+    }
+    if (Person.overlapsWith(cherry)) {
+        info.changeScoreBy(1)
+        cherry.destroy()
+    }
+})
+let chicken: Sprite = null
+let cherry: Sprite = null
+let Person: Sprite = null
+Person = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Food)
+controller.moveSprite(Person)
+cherry = sprites.create(img`
+    . . . . . . . . . . . 6 6 6 6 6 
+    . . . . . . . . . 6 6 7 7 7 7 8 
+    . . . . . . 8 8 8 7 7 8 8 6 8 8 
+    . . e e e e c 6 6 8 8 . 8 7 8 . 
+    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+    e 2 e e 2 2 2 2 e e e e c 6 8 . 
+    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+    . . . e c c e c 2 2 2 2 2 2 2 e 
+    . . . . . . . c 2 e e 2 2 e 2 c 
+    . . . . . . . c e e e e e e 2 c 
+    . . . . . . . . c e 2 2 2 2 c . 
+    . . . . . . . . . c c c c c . . 
+    `, SpriteKind.Food)
+cherry.setPosition(25, 45)
+chicken = sprites.create(img`
+    ..........bbbbbb................
+    .......bbb444444bb..............
+    .....2244444ddd444b.............
+    ....244444444dddd44e............
+    ...244444444444ddd4be...........
+    ..244444444444444d44be..........
+    .2b444444444444444d4be..........
+    .2b44444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bb4b4444444444444444bbe........
+    2bb4444444444444444444be........
+    2bb44444444444444444444e........
+    2bbb444bbb4444444444444e........
+    22bbb444bb4bb444444444be........
+    .2bbbbb44bbbb44444444bbe........
+    .22bbbbbbbb44bbb444444bbe.......
+    ..eeebbbbbbb44bbb444444be.......
+    ...eeeeebbbbbbbb44b4444be.......
+    .....eeeeee222bb44bbb4bbe.......
+    .......eeeee222bb44bbbbee.......
+    ............e222bbbbbbbec.......
+    ..............ee2bbbbeebdb......
+    .................eeeeecdddb.....
+    .......................cd11bbbb.
+    ........................cd111dbb
+    .........................b11111c
+    .........................c11dd1c
+    .........................cd1dbc.
+    .........................cb11c..
+    ..........................ccc...
+    ................................
+    `, SpriteKind.Food)
+chicken.setPosition(120, 80)
